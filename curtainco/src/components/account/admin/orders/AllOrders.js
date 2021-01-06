009f4a18255a3eed430701eb34004a02c226300f
philrussel21 <phil.antiporda21@gmail.com>
2021-01-06 20:49:19 +0930

    Adds modal viewing functionality to admin dashboard- orders section

--- curtainco/src/components/account/admin/orders/AllOrders.js
+++ curtainco/src/components/account/admin/orders/AllOrders.js
@@ -1,60 +1,60 @@
-import React, { useEffect } from "react"
+import React, { useEffect } from "react";
 
-import Checkbox from "@material-ui/core/Checkbox"
-import Table from "@material-ui/core/Table"
-import TableBody from "@material-ui/core/TableBody"
-import TableCell from "@material-ui/core/TableCell"
-import TableHead from "@material-ui/core/TableHead"
-import TableRow from "@material-ui/core/TableRow"
-import Paper from "@material-ui/core/Paper"
-import Button from "@material-ui/core/Button"
-import Title from "../../../reusable/Title"
+import Checkbox from "@material-ui/core/Checkbox";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Paper from "@material-ui/core/Paper";
+import Button from "@material-ui/core/Button";
+import Title from "../../../reusable/Title";
 
-import useStyles from "../AdminStyles"
+import useStyles from "../AdminStyles";
 import {
     getFirstNameFromFullName,
     getLastNameFromFullName,
-} from "../../../../helpers/userHelpers"
-import { displayShortDate } from "../../../../helpers/appHelpers"
-import { useCurtainContext } from "../../../../config/CurtainCoContext"
-import { ACTIONS } from "../../../../config/stateReducer"
+} from "../../../../helpers/userHelpers";
+import { displayShortDate } from "../../../../helpers/appHelpers";
+import { useCurtainContext } from "../../../../config/CurtainCoContext";
+import { ACTIONS } from "../../../../config/stateReducer";
 import {
     getAllOrders,
     markOrderProcessed,
-} from "../../../../services/orderServices"
+} from "../../../../services/orderServices";
 
 function AllOrders() {
-    const classes = useStyles()
-    const { state, dispatch } = useCurtainContext()
-    let allOrders = state.orders
+    const classes = useStyles();
+    const { state, dispatch } = useCurtainContext();
+    let allOrders = state.orders;
 
     useEffect(() => {
         getAllOrders()
             .then((resp) => {
-                console.log("---ORDERS---")
-                console.log(resp.data)
+                console.log("---ORDERS---");
+                console.log(resp.data);
                 dispatch({
                     type: ACTIONS.SET_ALL_ORDERS,
                     payload: resp.data,
-                })
+                });
             })
             .catch((err) => {
-                console.log(err)
-            })
-    }, [dispatch])
+                console.log(err);
+            });
+    }, [dispatch]);
 
     function handleOrderCheckbox(event) {
-        const checked = event.target.checked
-        const orderId = event.currentTarget.parentNode.parentNode.id
+        const checked = event.target.checked;
+        const orderId = event.currentTarget.parentNode.parentNode.id;
         markOrderProcessed(orderId, { isProcessed: checked })
             .then((resp) => {
-                console.log("---UPDATED ORDER---")
-                console.log(resp.data)
+                console.log("---UPDATED ORDER---");
+                console.log(resp.data);
                 if (resp.status === 200) {
                     dispatch({
                         type: ACTIONS.UPDATE_ORDER,
                         payload: resp.data,
-                    })
+                    });
                     dispatch({
                         type: ACTIONS.SET_SNACKBAR,
                         payload: {
@@ -62,16 +62,34 @@ function AllOrders() {
                             success: "success",
                             message: "Order successfully updated",
                         },
-                    })
+                    });
                 } else {
-                    console.log("OrderID provided does not exist")
+                    console.log("OrderID provided does not exist");
                 }
             })
             .catch((error) => {
                 console.log(
                     `Something went wrong when updating the order: ${error}`
-                )
-            })
+                );
+            });
+    }
+
+    function handleOrderSummary(event) {
+        // event.currentTarget.value = "fullName,message"
+        // const fullName = event.currentTarget.value.split("/")[0];
+        // const message = event.currentTarget.value.split("/")[1];
+        // prettier-ignore
+        const orderId = event.currentTarget.parentNode.parentNode.id;
+        const order = state.orders.find(ord => ord._id === orderId);
+        const title = `Order No: ${orderId}`;
+        dispatch({
+            type: ACTIONS.SET_MODAL,
+            payload: {
+                open: true,
+                data: order,
+                paymentSummary: true
+            },
+        });
     }
 
     const orderRow = allOrders.map((ord) => (
@@ -92,9 +110,8 @@ function AllOrders() {
             <TableCell
                 className={ord.isProcessed ? classes.checkboxSelected : ""}
             >
-                {/* {`${getFirstNameFromFullName(ord.customer.fullName)} 
-                ${getLastNameFromFullName(ord.customer.fullName)}`} */}
-                {ord.customer.fullName}
+                {`${getFirstNameFromFullName(ord.customer.fullName)} 
+                ${getLastNameFromFullName(ord.customer.fullName)}`}
             </TableCell>
             <TableCell
                 className={ord.isProcessed ? classes.checkboxSelected : ""}
@@ -110,14 +127,14 @@ function AllOrders() {
                 <Button
                     variant="outlined"
                     color="primary"
-                    // onClick={handleMessageButton}
+                    onClick={handleOrderSummary}
                     value={`${ord.customer.fullName}`}
                 >
                     View
                 </Button>
             </TableCell>
         </TableRow>
-    ))
+    ));
     return (
         <Paper className={classes.paper}>
             <Title>All Orders</Title>
@@ -135,7 +152,7 @@ function AllOrders() {
                 <TableBody>{orderRow}</TableBody>
             </Table>
         </Paper>
-    )
+    );
 }
 
-export default AllOrders
+export default AllOrders;
