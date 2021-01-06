32ceb7f5aa4b53b9751359fb54684b0caab6b292
philrussel21 <phil.antiporda21@gmail.com>
2021-01-06 16:25:25 +0930

    Adds Order Component services for making requests to the API

---
+++ curtainco/src/services/orderServices.js
@@ -0,0 +1,28 @@
+import api from "../config/api";
+
+// GET all orders
+async function getAllOrders() {
+  const response = await api.get("/orders");
+  console.log(response);
+  return response;
+}
+
+// POST new order
+async function createOrder(newOrder) {
+  const response = await api.post("/orders", newOrder);
+  return response;
+}
+
+// GET single order
+
+// PUT updated order
+async function markOrderProcessed(id, updatedOrder) {
+  const response = await api.patch(`/consults/${id}`, updatedOrder);
+  return response;
+}
+
+export {
+  getAllOrders,
+  createOrder,
+  markOrderProcessed
+};
