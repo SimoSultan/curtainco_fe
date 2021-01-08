407f866dcb48a3c1f3df3a189fcd38dc08547757
SimoSultan <simosultan2020@gmail.com>
2021-01-08 22:21:49 +1000

    deploy with live server

--- curtainco/src/config/api.js
+++ curtainco/src/config/api.js
@@ -1,9 +1,10 @@
-import axios from 'axios'
+import axios from "axios"
 
 // Create an axios instance
 export default axios.create({
-    baseURL: 'http://localhost:5000/api',
+    // baseURL: "http://localhost:5000/api", // development
+    baseURL: "https://curtainco.herokuapp.com/api", // production
     //   baseURL: 'heroku server',
     withCredentials: true,
-    timeout: 5000
+    timeout: 5000,
 })
